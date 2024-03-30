051f3c56632706b5aff52d0c49f86f41b59ae2b3
kumaramardeep342 <kumaramardeep342@gmail.com>
2024-03-31 00:40:59 +0530

    Basics-of-Implementation_3

--- password.py
+++ password.py
@@ -1,1 +1,18 @@
+###############################
+# probelm no. - 05            #
+# The variant of passwords    #
+# password.py                 #
+##############################
 
+# Reading input from STDIN 
+testcase = int(input())
+# print(testcase)
+for i in range (testcase):
+    details = input()
+    details=details.split()
+    details = [int(num) for num in details]
+    # print(details)
+    password = input()
+    # print(password)
+# Writing output to STDOUT
+         
